46696494b224263dd5148ead5777b73ea106fc75
gamersarmy03 <gamersarmy03@gmail.com>
2025-03-27 19:56:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openProfilePage() {
-    if (currentUser) {
-        localStorage.setItem('profileData', JSON.stringify(currentUser));
-        window.open('profile.html', '_blank');
-    }
-}
-
-document.getElementById('profilePic').addEventListener('click', openProfilePage);
